5d756645168a16de2b6533ef4c9b4f3855c884c8
Jean-Yves <jy.babiarczyk@gmail.com>
2023-07-19 11:16:17 +0200

    Error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,62 @@
+import { useEffect, useState } from "react";
 import loader from "./assets/loader.svg";
 import "./App.css";
+import browser from "./assets/browser.svg";
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
 
 function App() {
-  return <main>
-    <div className="loader-container">
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorInfo, setErrorInfo] = useState(null);
+
+  useEffect(() => {
+    
+    fetch(`http://api.airvisual.com/v2/nearest0_city?key=${APIKEY}`)
+      .then (response =>{
+        // console.log(response);
+        // 400-499 : Erreur Client
+        // 500-599 : Erreur Serveur
+        if(!response.ok) throw new Error(`Error ${response.status}, ${response.statusText}`);
+        return response.json()
+      })
+      .then(responseData => {
+        setWeatherData({
+          city: responseData.data.city,
+          country: responseData.data.country,
+          iconID: responseData.data.current.weather.ic,
+          temperature: responseData.data.current.weather.tp,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        console.dir(err)
+        setErrorInfo(err.message)
+      })
+  }, [])
+
+  return (
+    <main>
+      <div className={`loader-container ${(!weatherData && !errorInfo ) && "active"} `}>
       <img src={loader} alt="loading icon" />
-    </div>
-    <p className="city-name">Paris</p>
-    <p className="country-name">France</p>
-    <p className="temperature">17°</p>
-    <div className="info-icon-container">
-      <img src="/icons/01d.svg" alt="weather icon" />
-    </div>
-  </main>;
+      </div>
+      {weatherData && (
+        <>
+          <p className="city-name">{weatherData.city}</p>
+          <p className="country-name">{weatherData.country}</p>
+          <p className="temperature">{weatherData.temperature}°</p>
+          <div className="info-icon-container">
+              <img src={`/icons/${weatherData.iconID}.svg`} className="info-icon" alt="weather icon" />
+          </div>
+        </>
+        
+      )}
+      {(errorInfo && !weatherData) && (
+        <>
+          <p className="error-information">{errorInfo}</p>
+          <img src={browser} alt="error icon" />
+        </>
+      )}
+  </main>
+  )
 }
 
 export default App;
